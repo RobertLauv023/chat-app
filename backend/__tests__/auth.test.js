4c2b9ab104f5263794bf50a173001a8d3f4ebbb5
Robert Lauv <rlauv@cecs.pdx.edu>
2025-03-02 14:04:28 -0800

    Added backend testing for /auth routes. Modifed backend server file

---
+++ backend/__tests__/auth.test.js
@@ -0,0 +1,317 @@
+import request from "supertest"; // Supertest allows us to simulate HTTP requests
+import { app, connectDB, User } from "../app.js";
+import mongoose from "mongoose";
+import jsonwebtoken from 'jsonwebtoken';
+
+/**
+ * We rely on @shelf/jest-mongodb to supply process.env.MONGO_URL,
+ * which points to an in-memory MongoDB instance for testing.
+ *
+ * jest.config.cjs references @shelf/jest-mongodb as a preset,
+ */
+
+/**
+ * This describe block groups tests for the auth endpoints endpoint: 
+ * POST /api/auth/signup
+ * POST /api/auth/login
+ * POST /api/auth/logout
+ */
+describe("POST /api/auth", () => {
+    let token
+  /**
+   * Before any of our tests run, we connect Mongoose to the ephemeral MongoDB
+   * instance provided by process.env.MONGO_URL. The connectDB function is
+   * defined in `app.js`, so we just call it here.
+   */
+  beforeAll(async () => {
+    await connectDB(process.env.MONGO_URL);
+  });
+
+  /**
+   * After all tests complete, we disconnect from MongoDB to free resources
+   * and ensure no open handles remain (important in Jest).
+   */
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  /**
+   * We clear out all users between tests so each test starts with a fresh DB.
+   * This helps prevent one test's data from affecting another.
+   */
+  afterEach(async () => {
+    await User.deleteMany({});
+  });
+
+  /**
+   * 1) Test the success scenario:
+   *    If the request has valid email and password, the endpoint should
+   *    create a user and return status 201 with a success message.
+   */
+  it("should create a user and return 201 if email/password are valid", async () => {
+    /**
+     * We use supertest to POST to /api/auth/signup on our Express `app`.
+     * The `send({...})` method includes the JSON body with email/password.
+     */
+    const res = await request(app)
+      .post("/api/auth/signup")
+      .send({ email: "john@example.com", password: "secret123" });
+
+    // We expect a 201 (Created) status code
+    expect(res.status).toBe(201);
+    // We also expect the response body to have a message with success text
+    expect(res.body.message).toBe("User registered successfully");
+
+    /**
+     * Verify in the database that the user actually got created.
+     * We query the User model for the given email and ensure it's not null.
+     */
+    const userInDb = await User.findOne({ email: "john@example.com" });
+    expect(userInDb).not.toBeNull();
+  });
+
+  /**
+   * 2) Test the scenario where the email is already registered:
+   *    In that case, the endpoint should return a 400 (Bad Request)
+   *    and a message indicating the email is taken.
+   */
+  it("should return 409 if email is already registered", async () => {
+    // First, manually create a user document in the in-memory DB
+    await User.create({ email: "jane@example.com", password: "pass" });
+
+    // Now, attempt to sign up again with the same email
+    const res = await request(app)
+      .post("/api/auth/signup")
+      .send({ email: "jane@example.com", password: "newpass" });
+
+    // Expect 400 status code for a duplicate email scenario
+    expect(res.status).toBe(409);
+    // And check the message for "Email already registered"
+    expect(res.body.message).toBe("Email already registered");
+  });
+
+  /**
+   * 3) Test the scenario where the request is missing an email or a password:
+   *    The endpoint should respond with a 400 for either missing field.
+   */
+  it("should return 400 if email or password is missing", async () => {
+    // Missing password
+    let res = await request(app)
+      .post("/api/auth/signup")
+      .send({ email: "missingpass@example.com" });
+    expect(res.status).toBe(400);
+
+    // Missing email
+    res = await request(app)
+      .post("/api/auth/signup")
+      .send({ password: "newpass" });
+    expect(res.status).toBe(400);
+  });
+
+  /**
+   * 4) Test the success scenario:
+   *    Test the scenario where the login was successful
+   *    The endpoint should respond with a 200 and a success message
+   */
+  it("should return 200 if login was successful", async () => {
+    // First manually create the user-document for in memory DB
+    const ser = await request(app)
+      .post("/api/auth/signup")
+      .send({ email: "trial@example.com", password: "secret123" });
+
+    const res = await request(app)
+    .post("/api/auth/login")
+    .send({ email: "trial@example.com", password: "secret123" });
+  
+
+  // We expect a (200) Login success code
+  expect(res.status).toBe(200);
+  // And check the message for "Login successful!"
+  expect(res.body.message).toBe("Login successful!");
+  });
+
+  /**
+   * 5) Test the scenario where the request is missing an email or a password:
+   *    The endpoint should respond with a 400 for either missing field.
+   */
+  it("should return 400 if email or password is missing", async () => {
+    // Missing password
+    let res = await request(app)
+      .post("/api/auth/login")
+      .send({ email: "missingpass@example.com" });
+    expect(res.status).toBe(400);
+
+    // Missing email
+    res = await request(app)
+      .post("/api/auth/login")
+      .send({ password: "newpass" });
+    expect(res.status).toBe(400);
+  });
+
+  /**
+   * 6) Test the scenario where the request send an email that doesn't exist:
+   *    The endpoint should respond with a 400 for either missing field.
+   */
+  it("should return 404 if email entered is not found", async () => {
+    // Missing password
+    let res = await request(app)
+      .post("/api/auth/login")
+      .send({ email: "notrealemail@example.com", password: "fakepass123" });
+    expect(res.status).toBe(404);
+    expect(res.body.message).toBe("Email does not exist");
+
+  });
+
+  /**
+   * 7) Test the success scenario:
+   *    Test the scenario where the logout was successful
+   *    The endpoint should respond with a 200 and a success message.
+   */
+  it("should return 200 if logout was successful", async () => {
+    // Missing password
+    let res = await request(app)
+      .post("/api/auth/logout")
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe("Logout successful!");
+  });
+
+  /**
+   * 10) Test the success scenario:
+   *    Test the scenario where the update profile was successful
+   *    The endpoint should respond with a 200 and a success message.
+   */
+  it("should return 200 if profile updated", async () => {
+    // Manually create user in mongodb
+    const ser = await request(app)
+      .post("/api/auth/signup")
+      .send({ email: "trial@example.com", password: "secret123" });
+
+    // Login to get the JWT token
+    const response = await request(app)
+      .post("/api/auth/login")
+      .send({ email: "trial@example.com", password: "secret123" });
+       token = response.body.token;
+
+    // Test updating profile w/ firstName and lastName
+    const res = await request(app)
+      .post("/api/auth/update-profile")
+      .send({ firstName: "Testing", lastName: "Stone"})
+      .set('Authorization', `Bearer ${token}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body.message).toBe( "Profile updated!" )
+  });
+
+  /**
+   * 11) 
+   *    Test the scenario where firstName or lastName fields are missing
+   *    The endpoint should respond with a 400 and a error message.
+   */
+  it("should return 200 if profile updated", async () => {
+    // Manually create user in mongodb
+    const ser = await request(app)
+      .post("/api/auth/signup")
+      .send({ email: "trial@example.com", password: "secret123" });
+
+    // Login to get the JWT token
+    const response = await request(app)
+      .post("/api/auth/login")
+      .send({ email: "trial@example.com", password: "secret123" });
+       token = response.body.token;
+
+    // Test updating profile w/ firstName and lastName
+    let res = await request(app)
+      .post("/api/auth/update-profile")
+      .send({ firstName: "Testing"})
+      .set('Authorization', `Bearer ${token}`);
+      expect(res.status).toBe(400);
+      expect(res.body.message).toBe( "Missing first name or last name fields" )
+
+      res = await request(app)
+      .post("/api/auth/update-profile")
+      .send({ lastName: "Testing"})
+      .set('Authorization', `Bearer ${token}`);
+      expect(res.status).toBe(400);
+      expect(res.body.message).toBe( "Missing first name or last name fields" )
+    
+  });
+});
+
+describe("GET /api/auth", () => {
+    let token;
+    let decoded;
+    /**
+     * Before any of our tests run, we connect Mongoose to the ephemeral MongoDB
+     * instance provided by process.env.MONGO_URL. The connectDB function is
+     * defined in `app.js`, so we just call it here.
+     */
+    beforeAll(async () => {
+      await connectDB(process.env.MONGO_URL);
+    });
+  
+    /**
+     * After all tests complete, we disconnect from MongoDB to free resources
+     * and ensure no open handles remain (important in Jest).
+     */
+    afterAll(async () => {
+      await mongoose.disconnect();
+    });
+  
+    /**
+     * We clear out all users between tests so each test starts with a fresh DB.
+     * This helps prevent one test's data from affecting another.
+     */
+    afterEach(async () => {
+      await User.deleteMany({});
+    });
+
+    /**
+   * 8) Test the success scenario:
+   *    Test the scenario where the logout was successful
+   *    The endpoint should respond with a 200 and a success message.
+   */
+  it("should return 200 if user data found", async () => {
+    // Manually create user in mongodb
+    const ser = await request(app)
+      .post("/api/auth/signup")
+      .send({ email: "trial@example.com", password: "secret123" });
+
+    const response = await request(app)
+      .post("/api/auth/login")
+      .send({ email: "trial@example.com", password: "secret123" });
+       token = response.body.token;
+
+    const res = await request(app)
+      .get("/api/auth/userinfo")
+      .set('Authorization', `Bearer ${token}`);
+
+      expect(res.status).toBe(200);
+  });
+
+  /**
+   * 9) 
+   *    Test the scenario where user token invalid or user doesn't exist
+   *    The endpoint should respond with a 404 and an error message.
+   */
+  it("should return 200 if user data found", async () => {
+    const ser = await request(app)
+      .post("/api/auth/signup")
+      .send({ email: "trial@example.com", password: "secret123" });
+
+    const response = await request(app)
+      .post("/api/auth/login")
+      .send({ email: "trial@example.com", password: "secret123" });
+       token = response.body.token;
+
+    decoded = jsonwebtoken.decode(token, { complete: true});
+
+    decoded.payload.sub = "changed_value";
+
+    const res = await request(app)
+      .get("/api/auth/userinfo")
+      .set('Authorization', `Bearer ${decoded}`);
+
+      expect(res.status).toBe(404);
+      expect(res.body.message).toBe( "Profile not found" );
+  });
+});
---
+++ backend/app.js
@@ -0,0 +1,313 @@
+import express, { Router } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import * as bcrypt from 'bcrypt'
+import jsonwebtoken from 'jsonwebtoken';
+
+const secretKey = 'ALDNVBLSIKEJ123KFJ#$K!KJFDLK@J#!'
+const expiresIn = '1h';
+
+dotenv.config(); // Load environment variables from .env file
+
+const app = express(); // Create an Express application
+const PORT = process.env.PORT || 8747; // Define the port number (from environment variables or default 8747)
+const DATABASE_URL = process.env.DATABASE_URL; // Load MongoDB connection string from environment variables
+
+// Middleware to enable CORS (Cross-Origin Resource Sharing)
+app.use(
+  cors({
+    origin: process.env.ORIGIN, // Allow requests only from this frontend domain (defined in .env)
+    credentials: true, // Allow cookies and authentication headers in cross-origin requests
+  })
+);
+
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+
+// Define a user schema for MongoDB
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true }, // Email is required and must be unique
+  password: { type: String, required: true }, // Password is required
+});
+
+// Create a Mongoose model for the "users" collection
+export const User = mongoose.model("User", userSchema);
+
+
+const userProfileSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  email: { type: String},
+  firstName: { type: String },
+  lastName: { type: String },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+export const UserProfile = mongoose.model('UserProfile', userProfileSchema);
+
+const signup = async (req, res) => {
+  try {
+    console.log("Received signup request:", req.body); // Log incoming request body
+
+    // Extract email and password from request body
+    const { email, password } = req.body;
+
+    // Validate that email and password are provided
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+
+    // Check if the user already exists in the database
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: "Email already registered" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user document
+    const newUser = new User({ email, password: hashedPassword });
+    const savedUser = await newUser.save(); // Save the user to MongoDB
+
+    const userProfile = new UserProfile({ userId: savedUser._id, email: savedUser.email });
+    await userProfile.save();
+
+    console.log("User saved successfully:", savedUser);
+    res.status(201).json({ message: "User registered successfully" }); // Send success response
+  } catch (error) {
+    console.error("Signup error:", error);
+    res.status(500).json({ message: "Internal Server Error" }); // Send error response
+  }
+};
+
+// Authenticates a user logging in
+const login = async (req, res) => {
+  try {
+    console.log("Received login request:", req.body); // Log incoming request body
+
+    // Extract email and password from request body
+    const { email, password } = req.body;
+
+    // Validate that email and password are provided
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+
+    // Check if the user already exists in the database
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.status(404).json({ message: "Email does not exist" });
+    }
+
+    const storedHashPassword = existingUser.password;
+
+    const isValid = await bcrypt.compare(password, storedHashPassword);
+
+    if (isValid) {
+      console.log("Existing user id: " + existingUser._id);
+      const token = jsonwebtoken.sign({ id: existingUser._id, username: existingUser.email}, secretKey, { expiresIn: expiresIn});
+      console.log("User successfully logged in:", User);
+      res.status(200).json({ message: "Login successful!", token: token }); // Send success response
+    } else {
+      console.log("User failed to login")
+      res.status(400).json({ message: "Missing email or password, or invalid password"})
+    }
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ message: "Internal Server Error" }); // Send error response
+  }
+};
+
+// Logs the user out 
+const logout = async (req, res) => {
+  try {
+    console.log("Received logout request:"); // Log incoming request body
+
+    console.log("User successfully logged out:", User);
+    res.status(200).json({ message: "Logout successful!" }); // Send success response
+    } catch (error) {
+    console.error("Logout error:", error);
+    res.status(500).json({ message: "Internal Server Error" }); // Send error response
+  }
+};
+
+// Displays user first and last name
+const userinfo = async (req, res) => {
+
+  const token = req.headers['Authorization'] || req.headers['authorization'].split(' ')[1];;
+  if (!token) return res.status(404).json({ message: "User does not exist or ID not in token"});
+
+  try {
+    console.log("Received get user info request:"); // Log user info get request
+
+    const decoding = jsonwebtoken.decode(token);
+
+    const decoded = jsonwebtoken.verify(token, secretKey);
+    const userId = decoded.id;
+
+    const userProfile = await UserProfile.findOne({ userId });
+    if (!userProfile) return res.status(404).json({ message: "Profile not found" });
+
+    console.log("User profile found");
+    res.status(200).json({ message: "User data found", userProfile })
+
+  } catch (error) {
+    console.error("userinfo error:", error);
+    res.status(500).json({ message: "Unexpected server error" });
+  }
+};
+
+// Updates user first and last name
+const updateProfile = async (req, res) => {
+ 
+  const token = req.headers['Authorization'] || req.headers['authorization'].split(' ')[1];;
+  
+  if (!token) return res.status(404).json({ message: "Token not found" });
+
+  try {
+    console.log("Received update profile request: ");
+
+    const decoding = jsonwebtoken.decode(token);
+
+    const decoded = jsonwebtoken.verify(token, secretKey);
+    const userId = decoded.id;
+
+    if (!userId) {
+      console.log("Missing user ID");
+      res.status(400).json({ message: "Missing user ID or required fields "});
+      return;
+    }
+
+    const { firstName, lastName } = req.body;
+
+    if (!firstName || !lastName) {
+        console.log("Missing firstName or lastName ");
+        res.status(400).json({ message: "Missing first name or last name fields" });
+        return;
+    }
+
+    const updatedProfile = await UserProfile.findOneAndUpdate( 
+      { userId },
+      { firstName, lastName, updatedAt: new Date() },
+      { new: true }
+    );
+
+    console.log("User profile updated!", updatedProfile);
+    res.status(200).json({ message: "Profile updated!"});    
+  } catch (error) {
+    console.log("Update profile error: ", error);
+    res.status(500).json({ message: "Internal server error "});
+  }
+};
+
+// Search for users
+const search = async (req, res) => {
+  const { query } = req.query;
+
+  try {
+    console.log("Received search request: ", req.body);
+
+    const { searchTerm } = req.body;
+
+    if (!searchTerm) {
+      console.log("Missing search term");
+      res.status(400).json({ message: "Missing search term" });
+    } else {
+
+    const regex = new RegExp(searchTerm, 'i');
+
+    const user_search = await UserProfile.find({
+      $or: [
+        { firstName: { $regex: regex } }, 
+        { lastName: { $regex: regex } }
+      ]
+    }).exec();
+
+    
+    if (user_search.length === 0) {
+      console.log("No users found");
+      res.status(400).json([]);
+    } else {
+      console.log("Users found");
+      res.status(200).json(user_search);
+    }
+  }
+
+  } catch (error) {
+    console.log("User search error ", error);
+    res.status(500).json({ message: "Unexpected server error" });
+  }
+};
+
+// Displays all users except self
+const allContacts = async (req, res) => {
+  
+  const token = req.headers['Authorization'] || req.headers['authorization'].split(' ')[1];;
+
+  if (!token) return res.status(404).json({ message: "Token not found" });
+
+  try {
+    console.log("Received get all users request: ");
+
+    const decoded = jsonwebtoken.verify(token, secretKey);
+    const userId = decoded.id;
+
+    const user_search = await UserProfile.find({
+      userId: { $ne: userId } 
+    }).exec();
+
+    console.log("Users found");
+    res.status(200).json(user_search);
+
+  } catch (error) {
+    console.log("Get all users error", error);
+    res.status(500).json({ message: "Unexpected server or database error "});
+  }
+};
+
+
+// Define the router and endpoint
+const authRoutes = Router();
+const contactRoutes = Router();
+app.use("/api/auth", authRoutes);
+app.use("/api/contacts", contactRoutes);
+
+// POST /api/auth/signup
+authRoutes.post("/signup", signup);
+
+// POST /api/auth/login
+authRoutes.post("/login", login);
+
+// POST /api/auth/logout
+authRoutes.post("/logout", logout);
+
+// GET /api/auth/userinfo
+authRoutes.get("/userinfo", userinfo);
+
+// POST /api/auth/update-profile
+authRoutes.post("/update-profile", updateProfile);
+
+// POST /api/contacts/search
+contactRoutes.post("/search", search);
+
+// GET /api/contacts/all-contacts
+contactRoutes.get("/all-contacts", allContacts);
+
+/**
+ * connectDB:
+ * A helper to connect to MongoDB. We'll call this in index.js (for real use)
+ * and in tests (for in-memory or environment-based DB).
+ */
+export async function connectDB(uri) {
+    return mongoose.connect(uri);
+  }
+  
+  /**
+   * Export the `app` so other files (like index.js or test files) can import it.
+   * We also export the `User` model if tests need it.
+   */
+export { app };
